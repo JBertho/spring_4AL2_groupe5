9ab954d1a0dee9ebc4fad22c3babe29c7c87a892
JBertho <james.bertho94@gmail.com>
2021-04-29 16:33:53 +0200

    zegzegza

--- src/main/java/com/gotta_watch_them_all/app/testController.java
+++ src/main/java/com/gotta_watch_them_all/app/testController.java
@@ -3,7 +3,9 @@ package com.gotta_watch_them_all.app;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 @RequestMapping("/api/media")
 public class testController {
 
